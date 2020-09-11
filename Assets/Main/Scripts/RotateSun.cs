662504f4ad128754a4b286671652a7b619d2147c
nimish <nimishsantosh107@icloud.com>
2020-09-11 19:06:42 +0530

    Init

---
+++ Assets/Main/Scripts/RotateSun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSun : MonoBehaviour
+{
+    public float x;
+    public float y;
+    public float z;
+    Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = new Vector3(x,y,z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(movement * Time.deltaTime);
+    }
+}
